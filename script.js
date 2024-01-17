445d84e0418de4c1ec1121bac39419c9b8b98b2a
dhairyash gupta <dhairyashgupta12@gmail.com>
2024-01-17 20:48:09 +0530

    object add

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 let name = "dhairyash";
 const TOKEN = "shfi8239282";
+
+// object
+
+const Person = {
+  name: "dhairyash",
+  age: 20,
+  isGraduate: false,
+  hobbies: ["coding", "reading"],
+};
